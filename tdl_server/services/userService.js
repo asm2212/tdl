cdc5efd9199eb68566922ce102931ed2b2c30235
asm2212 <asmareadmasu0@gmail.com>
2024-05-26 14:34:09 +0300

    create userservice

---
+++ tdl_server/services/userService.js
@@ -0,0 +1,15 @@
+const UserModel = require("../model/userModel");
+
+class UserService {
+    static async userRegister(email,password){
+        try {
+            const createUser = new UserModel({email,password});
+            return await createUser.save();
+        } catch (error) {
+            
+        }
+     
+    }
+}
+
+module.exports = UserService;
