4eb41b2892ed573b1ab42bc75dc5f283c5d6aef5
asm2212 <asmareadmasu0@gmail.com>
2024-05-26 12:34:23 +0300

    fix mongodb connection

--- tdl_server/config/db.js
+++ tdl_server/config/db.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
 const con = mongoose.createConnection("mongodb://127.0.0.1:27017/tdl", {
-  
+    
+});
+
+con.once('open',() =>{
+    console.log("connected to mongodb");
+});
+
+con.on('error',(error) => {
+    console.error("mongodb connection error:",error);
 });
 
 module.exports = con;
--- tdl_server/index.js
+++ tdl_server/index.js
@@ -3,15 +3,10 @@ const db = require("./config/db.js");
 const todoRoutes = require("./routes/todos.js");
 const UserModel = require("./model/userModel.js");
 
-
 const port = 3000;
-
-
-
 
 app.use("/todos",todoRoutes);
 app.use("/todos/create",todoRoutes);
-
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
