5e518eb7102e8b28c232ef21dc355060f7351323
asm2212 <asmareadmasu0@gmail.com>
2024-05-26 15:45:45 +0300

    hash password

--- tdl_server/services/userService.js
+++ tdl_server/services/userService.js
@@ -1,13 +1,20 @@
 const UserModel = require("../model/userModel");
+const bcrypt = require('bcrypt');
 
 class UserService {
-    static async userRegister(email,password){
+    static async userRegister(email, password) {
         try {
-            const createUser = new UserModel({email,password});
+            
+            const salt = await bcrypt.genSalt(10);
+            
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            
+            const createUser = new UserModel({ email, password: hashedPassword });
             return await createUser.save();
         } catch (error) {
+            throw new Error('Error registering user');
         }
-     
     }
 }
 
