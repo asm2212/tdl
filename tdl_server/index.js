2af25666b8709172ee08cb58427e93baac85a076
asm2212 <asmareadmasu0@gmail.com>
2024-05-29 08:56:06 +0300

    create tdl schema

--- tdl_server/index.js
+++ tdl_server/index.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 const db = require("./config/db.js");
-const UserModel = require("./model/userModel.js");
+
 
 const port = 3000;
 
---
+++ tdl_server/model/tdlModel.js
@@ -0,0 +1,28 @@
+const db = require("../config/db");
+const UserModel = require("../model/userModel");
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const tdlSchema = new Schema (
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: UserModel.modelName 
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+const tdlModel = db.model("tdl",tdlSchema);
+module.exports = tdlModel;
